cc2cf2b81e90171293e0461b1febdb0a6e419a66
jparrker <jasonkk.parker@gmail.com>
2022-05-22 12:31:14 -0400

    solutions

---
+++ calc_avg_array.js
@@ -0,0 +1,12 @@
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+
+
+function find_average(array) {
+  if (array.length === 0)  {return 0}
+  else {
+    return array.reduce((acc, c) => (acc + c)) / array.length
+    }
+  }
