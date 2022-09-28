b6c07c1ab56da63d2ccfed3eb330f10b141f3826
jparrker <102701917+jparrker@users.noreply.github.com>
2022-09-28 18:53:42 -0400

    solution

---
+++ 8kyu/strange_comparison.js
@@ -0,0 +1,5 @@
+//In JavaScript, there is a special case where strict comparison of the same variable returns false! //Try to find out what must be done to get such result!
+
+function findStrangeValue() {
+  return NaN
+}
