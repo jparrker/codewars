71f0397e69d517330ae00277c893860119381519
jparrker <102701917+jparrker@users.noreply.github.com>
2022-10-01 18:55:05 -0400

    solution

---
+++ 8kyu/lexicalThis.js
@@ -0,0 +1,20 @@
+//Lexical this
+
+// Complete the Person object, by completing the FillFriends function to fill the _friends Array for the person object.
+// Output
+// Return the person object with the Name, Friends and FillFriends function filled out which will fill the input into the Friends property.
+// Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+//SOLUTION:
+
+const Person = () => {
+  const person = {
+    _name: 'Leroy',
+    _friends: [],
+    fillFriends: function(f) {
+      this._friends.push(...f)
+    },
+  }
+
+  return person
+}
