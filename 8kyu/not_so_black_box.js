2b133ef5abc3ade699670c0025b473edda12333d
jparrker <102701917+jparrker@users.noreply.github.com>
2022-10-04 18:55:39 -0400

    solution

---
+++ 8kyu/not_so_black_box.js
@@ -0,0 +1,13 @@
+// Not so black box
+
+// The kata has been preloaded with a function called blackBox. Figure out what the function does.
+
+// You may need to think laterally!
+
+// SOLUTION:
+
+let message = blackBox + ''
+console.log(message)
+//blackBox('you call JSopenSesame(), you'll pass!'')
+
+blackBox(JSopenSesame());
